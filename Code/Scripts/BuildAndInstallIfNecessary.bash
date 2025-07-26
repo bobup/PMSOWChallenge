fb3c67e12248c78cb78aa7b392c692f43bdc8db6
bobup <bobup@acm.org>
2025-07-26 15:10:56 -0400

    Added a doc; small change to bash script.

--- Code/Scripts/BuildAndInstallIfNecessary.bash
+++ Code/Scripts/BuildAndInstallIfNecessary.bash
@@ -45,50 +45,44 @@ OWCPushDate=`ssh pacmasters@pacmasters.pairserver.com \
 	"( date -r $OWCFILE )"`
 echo "CURRENT_YEAR='$CURRENT_YEAR', OWPUSHDATE='$OWPUSHDATE', OWCPushDate='$OWCPushDate'"
 
-# if we can't find results for OW then we're going to use the previous year as the CURRENT_DATE. We do this
-# to handle the case where we're running on Jan 1 of the year following the last year of OW competitions.
-# It's possible that's not the case, and we're just getting some error trying to find the latest OW
-# results, but in that case we'll just ignore the current year. That's not 
-
-
-#pretend that the last OW results were created on 
-# Jan 1 of the current year. This will handle the case of the first run of a new year. In that case, it will
-# appear that the OW results have changed, thus a new OWChallenge page will be generated with the new year
-# in the title but with no other changes. The next time this script runs it will think that the OWChallenge page
-# is "newer" than the OW points page thus nothing will be generated. Eventually a new OW points page will be
-# generated causing a new OWChallenge page to be generated, as it should.
+# did we find the this year's latest OW Points page?
 if [ ".$OWPUSHDATE" == . ] ; then
+	# NO! In that case assume that we don't have an OW points page for this year (for example, we're executing
+	# this on Jan 1 of the year following the last year of OW points generation.) So we'll use the previous year.
 	CURRENT_YEAR=$(expr $CURRENT_YEAR - 1)
 	# full path of the OW Points page on the production server:
 	OWPOINTSFILE=$PRODDIRECTORY/OWPoints/${CURRENT_YEAR}PacMastersAccumulatedResults.html
 	OWPUSHDATE=`ssh pacmasters@pacmasters.pairserver.com \
 		"( date -r  $OWPOINTSFILE )"`
 	echo "Above changed. Now:  CURRENT_YEAR='$CURRENT_YEAR', OWPUSHDATE='$OWPUSHDATE', OWCPushDate='$OWCPushDate'"
-fi
-if [ ".$OWPUSHDATE" == . ] ; then
-	LogMessage "$SIMPLE_SCRIPT_NAME failed." \
-		"$(cat <<- BUp8
-		$SIMPLE_SCRIPT_NAME failed: unable to find latest OW results for $ORIGINAL_CURRENT_YEAR and $CURRENT_YEAR.
-		ORIGINAL_CURRENT_YEAR='$ORIGINAL_CURRENT_YEAR'
-		CURRENT_YEAR='$CURRENT_YEAR'
-		Date of last OW push for $CURRENT_YEAR: NONE FOUND!
-		Date of last OWChallenge generation: $OWCPushDate
-		Abort!!
-		BUp8
-		)"
-	exit 1;
-else
-	LogMessage "$SIMPLE_SCRIPT_NAME recovered from failure." \
-		"$(cat <<- BUp8
-		$SIMPLE_SCRIPT_NAME recovered: unable to find latest OW results for $ORIGINAL_CURRENT_YEAR but
-		  found them for $CURRENT_YEAR.  That's the results we're using.
-		ORIGINAL_CURRENT_YEAR='$ORIGINAL_CURRENT_YEAR'  (we are NOT using that year)
-		CURRENT_YEAR='$CURRENT_YEAR'  (we ARE using that year)
-		Date of last OW push for $CURRENT_YEAR: $OWPUSHDATE
-		Date of last OWChallenge generation: $OWCPushDate
-		Another email should follow...
-		BUp8
-		)"
+	# if by now we haven't found an OW Points page then we're going to throw an error and give up.
+	if [ ".$OWPUSHDATE" == . ] ; then
+		# OK - give up!
+		LogMessage "$SIMPLE_SCRIPT_NAME failed." \
+			"$(cat <<- BUp8
+			$SIMPLE_SCRIPT_NAME failed: unable to find latest OW results for $ORIGINAL_CURRENT_YEAR and $CURRENT_YEAR.
+			ORIGINAL_CURRENT_YEAR='$ORIGINAL_CURRENT_YEAR'
+			CURRENT_YEAR='$CURRENT_YEAR'
+			Date of last OW push for $CURRENT_YEAR: NONE FOUND!
+			Date of last OWChallenge generation: $OWCPushDate
+			Abort!!
+			BUp8
+			)"
+		exit 1;
+	else
+		# We've got an OW Points page from last year. We're going to use it after logging the situation.
+		LogMessage "$SIMPLE_SCRIPT_NAME recovered from failure." \
+			"$(cat <<- BUp8
+			$SIMPLE_SCRIPT_NAME recovered: unable to find latest OW results for $ORIGINAL_CURRENT_YEAR but
+			  found them for $CURRENT_YEAR.  That's the results we're using.
+			ORIGINAL_CURRENT_YEAR='$ORIGINAL_CURRENT_YEAR'  (we are NOT using that year)
+			CURRENT_YEAR='$CURRENT_YEAR'  (we ARE using that year)
+			Date of last OW push for $CURRENT_YEAR: $OWPUSHDATE
+			Date of last OWChallenge generation: $OWCPushDate
+			Another email should follow...
+			BUp8
+			)"
+	fi
 fi
 
 # is the date of the last push of OW points more recent than the date of the last push of OWChallenge?
@@ -97,17 +91,12 @@ DATEOFLASTOWPUSH=`date -d "$OWPUSHDATE" +%s`
 DATEOFLASTOWCPUSH=`date -d "$OWCPushDate" +%s`
 echo "DATEOFLASTOWPUSH='$DATEOFLASTOWPUSH', DATEOFLASTOWCPUSH='$DATEOFLASTOWCPUSH'"
 
-
-#if [ $DATEOFLASTOWPUSH -gt $DATEOFLASTOWCPUSH ] ; then
-
-if [ 1 ] ; then
-
-
+if [ $DATEOFLASTOWPUSH -gt $DATEOFLASTOWCPUSH ] ; then
 	# YES! regenerate the OWChallenge page
 	echo Regeneration of OWChallenge in progress...
 	./OWCGenerate -E"$CURRENT_YEAR"
-###	./PMSScripts/DevPushOWC.bash
-###	./PMSScripts/ProdPushOWC.bash
+	./PMSScripts/DevPushOWC.bash
+	./PMSScripts/ProdPushOWC.bash
 	echo Regeneration done.
 else
 	echo No Regeneration of OWChallenge necessary.
